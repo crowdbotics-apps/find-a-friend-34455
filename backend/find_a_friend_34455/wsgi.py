a0edaab2c46b6e07fdd0741ceb0bf1f50bd159bc
Crowdbotics <team@crowdbotics.com>
2022-03-27 05:48:15 +0000

    Update models

--- backend/find_a_friend_34455/wsgi.py
+++ backend/find_a_friend_34455/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'find_a_friend_34455.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "find_a_friend_34455.settings")
 
 application = get_wsgi_application()
